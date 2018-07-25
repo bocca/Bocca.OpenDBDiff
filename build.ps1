b09ed6b86111df679ef2cd14d5e729b927c3a4ef
Pablo Romero <promero@grupointermark.com>
2018-07-25 10:20:18 +0200

    Add title

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 if (-not (Test-Path "nuget.exe")) { Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "nuget.exe" }	
-./nuget.exe pack Bocca.OpenDBDiff.nuspec
+./nuget.exe pack Bocca.OpenDBDiff.nuspec -OutputDirectory out
